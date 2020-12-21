fabab342b368a6db9ef6d9873fda8ccb6a6604ca
MaxHwK <maxence.giron@gmail.com>
2020-12-21 19:04:35 +0100

    Ajout CSS pour insertArticle + updateArticle

--- src/Form/Type/ArticleType.php
+++ src/Form/Type/ArticleType.php
@@ -11,10 +11,10 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options):void
     {
         $builder
-            ->add('title', TextType::class, ['attr' => ['placeholder' => 'Titre..', 'class' => 'box-input', 'id' => 'titrenews', 'name' => 'titrenews']])
-            ->add('content', TextType::class, ['attr' => ['placeholder' => 'Saisir le contenu..', 'class' => 'box-text-area']])
-            ->add('idUser', TextType::class, ['attr' => ['placeholder' => 'ID de l\'utilisateur..', 'class' => 'box-input']])
-            ->add('srcImage', TextType::class, ['attr' => ['placeholder' => 'Source de l\'image..']])
+            ->add('title', TextType::class, ['attr' => ['placeholder' => 'Titre', 'class' => 'box-input']])
+            ->add('content', TextType::class, ['attr' => ['placeholder' => 'Contenu de l\'article', 'class' => 'box-input-art']])
+            ->add('idUser', TextType::class, ['attr' => ['placeholder' => 'ID du redacteur', 'class' => 'box-input']])
+            ->add('srcImage', TextType::class, ['attr' => ['placeholder' => 'Source de l\'image', 'class' => 'box-input']])
             ->add('save', SubmitType::class, ['label' => 'Confirmer', 'attr' => ['class' => 'add-button']]);
     }
 }
