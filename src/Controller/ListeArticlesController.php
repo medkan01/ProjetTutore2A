b91e8e1035e17bb26f2d68a6ecb1d97c455323ad
medkan01 <mehdiakniou@gmail.com>
2021-01-09 18:22:13 +0100

    affichage des articles avec les boutons

--- src/Controller/ListeArticlesController.php
+++ src/Controller/ListeArticlesController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
 
 class ListeArticlesController extends AbstractController
 {
     /**
-     * @Route("/liste/articles", name="liste_articles")
+     * @Route("/listeArticles", name="liste_articles")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
-        return $this->render('liste_articles/index.html.twig', [
-            'controller_name' => 'ListeArticlesController',
+        $articles = $repo->findAll();
+
+        return $this->render('liste_articles/listeArticles.html.twig', [
+            'controller_name' => 'ArticlesController',
+            'articles' => $articles
         ]);
     }
 }
