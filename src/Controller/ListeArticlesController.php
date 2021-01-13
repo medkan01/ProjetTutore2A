7f282e245df241303c76f76c89725c47e8799cf6
Asyjjan <gisonnisylvio@gmail.com>
2021-01-13 16:38:16 +0100

    Exportation fichier xml terminer

--- src/Controller/ListeArticlesController.php
+++ src/Controller/ListeArticlesController.php
@@ -10,11 +10,8 @@ use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Form\Type\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\DateTime;
 use XMLWriter;
-
-// Include Dompdf required namespaces
-use Dompdf\Dompdf;
-use Dompdf\Options;
 
 
 class ListeArticlesController extends AbstractController
@@ -49,17 +46,31 @@ class ListeArticlesController extends AbstractController
     function export(Article $article){
       
         $xml= new XMLWriter();
-        $xml->openUri("articles.xml");
+
+        $date = $article->getCreatedAt();
+        $result = $date->format('Y-m-d H:i:s');
+        $result2 = "pas de modification";
+
+        if($article->getUpdatedAt() != null){
+            $date = $article->getUpdatedAt();
+            $result2 = $date->format('Y-m-d H:i:s');
+        }
+
+        $xml->openUri("articles".$article->getId().".xml");
         $xml->startDocument('1.0', 'utf-8');
-        $xml->startElement('mesarticles');
-        $xml->startElement('article');
-        $xml->writeAttribute('id', $article['id']);
-        $xml->writeElement('titre',$article['title']);
-        $xml->writeElement('date',$article['created_at']);
-        $xml->writeElement('contenu',$article['content']);
+        $xml->startElement('Article');
+        $xml->writeElement('id', $article->getId());
+        $xml->writeElement('titre',$article->getTitle());
+        $xml->writeElement('datecreation',$result);
+        $xml->writeElement('datemodification',$result2);
+        $xml->writeElement('contenu',$article->getContent());
+        $xml->writeElement('image',$article->getSrcImage());
+        $xml->endElement();
+        $xml->endElement();
         $xml->endElement();
         $xml->endElement();
         $xml->endElement();
         $xml->flush();
+        return $this->redirectToRoute('liste_articles');
       }
 }
